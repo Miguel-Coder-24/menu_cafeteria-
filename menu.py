8a713a743293c197e5e2b2aae9767809d6afaae5
Miguel-24 <mortegonb@unal.edu.co>
2025-08-18 17:32:37 -0500

    Update menu.py

--- menu.py
+++ menu.py
@@ -39,7 +39,7 @@ menu = {
         ("Pancakes jamón y queso", 5000),
         ("Alfajores con nibs de cacao", 5000),
         ("Almojábana", 3000),
-        ("Empanadas (jamón y queso, pollo, carne, hawaianas", 3000),
+        ("Empanadas (jamón y queso, pollo, carne, hawaianas)", 3000),
         ("Maní garrapiñado", 3000),
         ("Caramelos artesanales", 3000),
         ("Mini galletas con nibs de cacao", 3000),
