6dfc4c7e6e29c90fc4ca0f0362892a6463421fad
Miguel-24 <mortegonb@unal.edu.co>
2025-08-18 20:23:20 -0500

    Update menu.py

--- menu.py
+++ menu.py
@@ -5,14 +5,14 @@ import pandas as pd
 # Datos del menú
 # =============================
 menu = {
-    "Bebidas calientes": [
-        ("Americano grande", 4000),
-        ("Americano pequeño", 2000),
+    "Bebidas calientes": [      
         ("Mocachino", 7000),
         ("Chocolate", 6000),
         ("Capuchino", 6000),
         ("Café con leche", 4000),
+        ("Americano grande", 4000),
         ("Expreso", 3000),
+        ("Americano pequeño", 2000),
         ("Aromáticas", 2000),
     ],
 
@@ -26,7 +26,7 @@ menu = {
     ],
 
     "Helados artesanales": [
-        ("Helado con brownie", 10000),
+        ("Brownie con helado", 10000),
         ("Helado 2 bolas cono", 7000),
         ("Helado 1 bola cono", 4000),
         ("Helado 1 bola vaso", 3500),
@@ -35,26 +35,28 @@ menu = {
 
     "Comidas y repostería": [
         ("Brownie de chocolate", 5000),
-        ("Pudín de (café, ciruela)", 5000),
-        ("Sándwich jamón y queso", 5000),
+        ("Pudínes (café, ciruela)", 5000),
+        ("Sándwiches jamón y queso", 5000),
         ("Pancakes jamón y queso", 5000),
         ("Alfajores con nibs de cacao", 5000),
-        ("Almojábana", 3000),
+        ("Almojábanas de queso", 3000),
         ("Empanadas (jamón y queso, pollo, carne, hawaianas)", 3000),
         ("Maní garrapiñado", 3000),
-        ("Caramelos artesanales", 3000),
+        ("Caramelos artesanales de cacao", 3000),
         ("Mini galletas con nibs de cacao", 3000),
-        ("Galletas de (café, ciruela, surtidas)", 2000),
-        ("Muffin de (café, ciruela)", 2000),
-        ("Dedos de queso", 1500),   
+        ("Galletas (café, ciruela, surtidas)", 2000),
+        ("Muffins (café, ciruela)", 2000),
+        ("Dedos de queso", 1500),
+        ("Muffins (ahuyama, cacao)", 3000),
+        ("Vaso minimuffins", 5000),
     ],
 
-    "Café y cacao en grano": [
-        ("Café de libra", 35000),
-        ("Café de media libra", 18000),
-        ("Café de 125 gramos", 10000),
-        ("Café orgánico libra", 55000),
-        ("Café orgánico media libra", 30000),
+    "Café y Cacao en grano": [
+        ("Café bolsa 500g", 35000),
+        ("Café bolsa 250g", 18000),
+        ("Café bolsa 125g", 10000),
+        ("Café orgánico 500g", 55000),
+        ("Café orgánico 250g", 30000),
         ("Chocolate puro 250g", 30000),
         ("Chocolate 70% 250g", 25000),
         ("Chocolate de bola 250g", 20000),
@@ -62,7 +64,7 @@ menu = {
         ("Chocolate mini 70%", 6000),
         ("Chocolate mini bola", 5000),
         ("Nibs de cacao naturales", 8000),
-        ("Nibs de cacao caramelizados", 8000),
+        ("Nibs de cacao caramelizados con panela", 8000),
     ]
 }
 
