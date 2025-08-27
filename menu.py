bcf7e9fee6dd783953dc5eb65fccb1facdacec14
Miguel-24 <mortegonb@unal.edu.co>
2025-08-27 16:54:26 -0500

    Update menu.py

--- menu.py
+++ menu.py
@@ -95,9 +95,9 @@ menu = {
 # =============================
 # Interfaz con Streamlit
 # =============================
-st.set_page_config(page_title="Menú de Cafe & Cacao", page_icon="☕", layout="wide")
+st.set_page_config(page_title="Menú de Cafe & Cacao", page_icon="☕🍫", layout="wide")
 
-st.title(" Menú de Cafe☕ & Cacao🍫")
+st.title(" Menú de Cafe ☕ & Cacao🍫")
 
 # Mostrar categorías con tabla HTML personalizada
 for categoria, productos in menu.items():
